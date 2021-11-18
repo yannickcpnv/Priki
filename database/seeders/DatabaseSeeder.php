8c30de17ad4ebf31c9e3d1fe67c38a29a2b55e78
Yannick Baudraz <yannick.baudraz@cpnv.ch>
2021-11-18 17:00:19 +0100

    Add seeders, factory and jetstream
    Seeders for :
      - Database (update)
      - Domain
      - PublicationState
      - Role
      - User
    
      Model for :
        - Role
    
      Factory for :
        - User

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+                        PublicationStateSeeder::class,
+                        DomainSeeder::class,
+                        RoleSeeder::class,
+
+                        UserSeeder::class,
+                    ]);
     }
 }
