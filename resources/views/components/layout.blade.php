4318b6606d1047dbf0487d82c77140139b853fe1
yannick <yannickbaudraz@gmail.com>
2021-12-02 07:44:11 +0100

    Change title page

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Priki</title>
+    <title>{{ $titlePage }}</title>
 
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     @livewireStyles
