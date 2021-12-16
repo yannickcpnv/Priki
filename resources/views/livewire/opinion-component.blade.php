4a114aa8d7cae1e088fa9a7a9232ac0d8db1b2b1
Yannick Baudraz <Yannick.BAUDRAZ@cpnv.ch>
2021-12-16 13:24:51 +0100

    Add link to author name

--- resources/views/livewire/opinion-component.blade.php
+++ resources/views/livewire/opinion-component.blade.php
@@ -1,6 +1,6 @@
 <article class="border-4 my-2">
     <div>Description : {{ $this->opinion->description }}</div>
-    <div>Auteur : {{ $this->opinion->user->name }}</div>
+    <div>Auteur : <a href="#">{{ $this->opinion->user->name }}</a></div>
     <div>Date de création : {{ $this->opinion->created_at->isoFormat('LL') }}</div>
     <div>Nombre de retours : {{ $this->opinion->users()->count() }}</div>
     <div>+ : / - : {{ $this->opinion->users()->sum('points') }}</div>
